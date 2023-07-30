99dd9f84aa698b35c5102a78cd71c7b13e819c0b
Divyansh Gupta <divyanshg809@gmail.com>
2023-07-30 22:10:48 +0530

    adapter update

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -11,10 +11,13 @@ let prisma: PrismaClient
 if (process.env.NODE_ENV === 'production') {
   prisma = new PrismaClient()
 } else {
-  if (!global.cachedPrisma) {
-    global.cachedPrisma = new PrismaClient()
+  let globalWithPrisma = global as typeof globalThis & {
+    prisma: PrismaClient;
+  };
+  if (!globalWithPrisma.prisma) {
+    globalWithPrisma.prisma = new PrismaClient();
   }
-  prisma = global.cachedPrisma
+  prisma = globalWithPrisma.prisma;
 }
 
 export const db = prisma
