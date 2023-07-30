0fa6699dd977c3629b13d37a7d787d277bb93481
Divyansh Gupta <divyanshg809@gmail.com>
2023-07-30 22:20:43 +0530

    new db user

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -11,13 +11,10 @@ let prisma: PrismaClient
 if (process.env.NODE_ENV === 'production') {
   prisma = new PrismaClient()
 } else {
-  let globalWithPrisma = global as typeof globalThis & {
-    prisma: PrismaClient;
-  };
-  if (!globalWithPrisma.prisma) {
-    globalWithPrisma.prisma = new PrismaClient();
+  if (!global.cachedPrisma) {
+    global.cachedPrisma = new PrismaClient()
   }
-  prisma = globalWithPrisma.prisma;
+  prisma = global.cachedPrisma
 }
 
 export const db = prisma
