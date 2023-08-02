34836b854085306e879cb07a9b0ccc3542429518
Divyansh Gupta <divyanshg809@gmail.com>
2023-08-02 15:51:33 +0530

    s

--- src/components/FRResults.tsx
+++ src/components/FRResults.tsx
@@ -1,11 +1,171 @@
 import { FC } from 'react';
 
+import { calculateScores } from '../constants/firo';
+
 interface FRResultsProps {
   responses: any;
 }
 
+const Td = ({ item, _key }: { item: string | number; _key?: string }) => (
+  <td className="w-[160px] divide-x divide-black space-x-4 flex items-center justify-center">
+    <span className="w-[80px] text-center">{item}</span>
+    <span className="pl-2 w-[80px] text-center">{_key}</span>
+  </td>
+);
+
+const Tr = ({
+  items,
+  _keys,
+}: {
+  _keys: string[] | any;
+  items: string[] | number[];
+}) => (
+  <tr className="flex flex-row space-x-6 ">
+    {items.map((it, idx) => (
+      <Td key={idx} item={it} _key={_keys[idx]} />
+    ))}
+  </tr>
+);
+
 const FRResults: FC<FRResultsProps> = ({ responses }) => {
-  return <div>FAResults</div>;
+  function getQuestionSet(questionNumbers: number[]) {
+    const result: { [key: string]: any } = {};
+
+    questionNumbers.forEach((questionNumber) => {
+      const questionKey = `question_${questionNumber}`;
+      if (questionKey in responses) {
+        result[questionKey] = responses[questionKey];
+      }
+    });
+
+    return result;
+  }
+  return (
+    <div>
+      <table>
+        {/* <thead>
+          <tr className="flex flex-row space-x-4 w-[160px]">
+            <td className="text-center">
+              Expressed
+              <br />
+              Inclusion
+            </td>
+            <td className="w-[160px] text-center">
+              Expressed
+              <br />
+              Inclusion
+            </td>
+            <td className="w-[160px] text-center">
+              Expressed
+              <br />
+              Inclusion
+            </td>
+            <td className="w-[160px] text-center">
+              Expressed
+              <br />
+              Inclusion
+            </td>
+            <td className="w-[160px] text-center">
+              Expressed
+              <br />
+              Inclusion
+            </td>
+            <td className="w-[160px] text-center">
+              Expressed
+              <br />
+              Inclusion
+            </td>
+          </tr>
+        </thead> */}
+        <thead className="flex flex-row space-x-6 border-t border-b border-black">
+          <tr className="flex flex-row space-x-4 w-[160px]">
+            <td className="w-[80px] text-center">Item</td>
+            <td className="w-[80px] text-center">Key</td>
+          </tr>
+          <tr className="flex flex-row space-x-4 w-[160px]">
+            <td className="w-[80px] text-center">Item</td>
+            <td className="w-[80px] text-center">Key</td>
+          </tr>
+          <tr className="flex flex-row space-x-4 w-[160px]">
+            <td className="w-[80px] text-center">Item</td>
+            <td className="w-[80px] text-center">Key</td>
+          </tr>
+
+          <tr className="flex flex-row space-x-4 w-[160px]">
+            <td className="w-[80px] text-center">Item</td>
+            <td className="w-[80px] text-center">Key</td>
+          </tr>
+          <tr className="flex flex-row space-x-4 w-[160px]">
+            <td className="w-[80px] text-center">Item</td>
+            <td className="w-[80px] text-center">Key</td>
+          </tr>
+          <tr className="flex flex-row space-x-4 w-[160px]">
+            <td className="w-[80px] text-center">Item</td>
+            <td className="w-[80px] text-center">Key</td>
+          </tr>
+        </thead>
+        <tbody>
+          <Tr
+            items={[1, 28, 30, 2, 4, 29]}
+            _keys={["1-2-3", "1-2", "1-2-3", "1-2-3-4", "1-2", "1-2"]}
+          />
+          <Tr
+            items={[3, 31, 33, 6, 8, 32]}
+            _keys={["1-2-3-4", "1-2", "1-2-3", "1-2-3-4", "1-2", "1-2"]}
+          />
+          <Tr
+            items={[5, 34, 36, 10, 12, 35]}
+            _keys={["1-2-3-4", "1-2", "1-2", "1-2-3", "1", "5-6"]}
+          />
+          <Tr
+            items={[7, 37, 41, 14, 17, 38]}
+            _keys={["1-2-3", "1", "1-2-3-4", "1-2-3", "1-2", "1-2"]}
+          />
+          <Tr
+            items={[9, 39, 44, 18, 19, 40]}
+            _keys={["1-2", "1", "1-2-3", "1-2-3", "4-5-6", "5-6"]}
+          />
+          <Tr
+            items={[11, 42, 47, 20, 21, 43]}
+            _keys={["1-2", "1-2", "1-2-3", "1-2-3", "1-2", "1"]}
+          />
+          <Tr
+            items={[13, 45, 50, 22, 23, 46]}
+            _keys={["1-2", "1-2", "1-2", "1-2-3-4", "1-2", "5-6"]}
+          />
+          <Tr
+            items={[15, 48, 53, 24, 25, 49]}
+            _keys={["1", "1-2", "1-2", "1-2-3", "4-5-6", "1-2"]}
+          />
+          <Tr
+            items={[16, 51, 54, 26, 27, 52]}
+            _keys={["1", "1-2", "1-2", "1-2-3", "1-2", "5-6"]}
+          />
+          <Tr
+            items={["Score", "Score", "Score", "Score", "Score", "Score"]}
+            _keys={[
+              calculateScores(getQuestionSet([1, 3, 5, 7, 9, 11, 13, 15, 16])),
+              calculateScores(
+                getQuestionSet([28, 31, 34, 37, 39, 42, 45, 48, 51])
+              ),
+              calculateScores(
+                getQuestionSet([30, 33, 36, 41, 44, 47, 50, 53, 54])
+              ),
+              calculateScores(
+                getQuestionSet([2, 6, 10, 14, 18, 20, 22, 24, 26])
+              ),
+              calculateScores(
+                getQuestionSet([4, 8, 12, 17, 19, 21, 23, 25, 27])
+              ),
+              calculateScores(
+                getQuestionSet([29, 32, 35, 38, 40, 43, 46, 49, 52])
+              ),
+            ]}
+          />
+        </tbody>
+      </table>
+    </div>
+  );
 };
 
 export default FRResults;
---
+++ src/constants/firo.ts
@@ -0,0 +1,77 @@
+type PossibleResponses = {
+  [key: number]: string;
+};
+const possibleResponses: PossibleResponses = {
+  "1": "1-2-3",
+  "2": "1-2-3-4",
+  "3": "1-2-3-4",
+  "4": "1-2",
+  "5": "1-2-3-4",
+  "6": "1-2-3-4",
+  "7": "1-2-3",
+  "8": "1-2",
+  "9": "1-2",
+  "10": "1-2-3",
+  "11": "1-2",
+  "12": "1",
+  "13": "1-2",
+  "14": "1-2-3",
+  "15": "1",
+  "16": "1",
+  "17": "1-2",
+  "18": "1-2-3",
+  "19": "4-5-6",
+  "20": "1-2-3",
+  "21": "1-2",
+  "22": "1-2-3-4",
+  "23": "1-2",
+  "24": "1-2-3",
+  "25": "4-5-6",
+  "26": "1-2-3",
+  "27": "1-2",
+  "28": "1-2",
+  "29": "1-2",
+  "30": "1-2-3",
+  "31": "1-2",
+  "32": "1-2",
+  "33": "1-2-3",
+  "34": "1-2",
+  "35": "5-6",
+  "36": "1-2",
+  "37": "1",
+  "38": "1-2",
+  "39": "1",
+  "40": "5-6",
+  "41": "1-2-3-4",
+  "42": "1-2",
+  "43": "1",
+  "44": "1-2-3",
+  "45": "1-2",
+  "46": "5-6",
+  "47": "1-2-3",
+  "48": "1-2",
+  "49": "1-2",
+  "50": "1-2",
+  "51": "1-2",
+  "52": "5-6",
+  "53": "1-2",
+  "54": "1-2",
+};
+
+export default possibleResponses;
+
+export function calculateScores(responses: any) {
+  let score: number = 0;
+  Object.keys(responses).forEach((question: any) => {
+    const _question = question.split("_")[1];
+    const responsesPossible = possibleResponses[_question]
+      ?.split("-")
+      .map(Number);
+
+    if (responsesPossible?.includes(responses[question])) {
+      score++;
+    }
+  });
+
+  return score;
+}
