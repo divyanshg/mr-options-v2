133124bc3f2bbc1fc40c656d731b2dd9a3d1f6f1
divyanshg <divyanshg809@gmail.com>
2023-08-28 23:57:59 +0530

    final url update

--- src/axios/index.ts
+++ src/axios/index.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-  baseURL: "http://mr-app.astrokleem.com:4000/api",
+  baseURL: "https://mr-app.astrokleem.in/api",
   headers: {
     "Content-Type": "application/json",
   },
