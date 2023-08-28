73ec56b55d1bb4f3ad94f0747b3f20a9b076e625
divyanshg <divyanshg809@gmail.com>
2023-08-28 23:15:36 +0530

    url change

--- src/axios/index.ts
+++ src/axios/index.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-  baseURL: "http://localhost:4000/api",
+  baseURL: "http://mr-app.astrokleem.com:4000/api",
   headers: {
     "Content-Type": "application/json",
   },
